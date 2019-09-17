2143305b287b874beb95ff7740625efcd8b10a81
julianandres <julian.andres@eventbrite.com>
2019-09-17 12:01:44 -0300

    EVB Login Config

--- supplier_management_site/settings/testing.py
+++ supplier_management_site/settings/testing.py
@@ -2,11 +2,12 @@ from .base import *  # noqa
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['testserver']
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlitetesting3'),
     }
 }
+
+SOCIAL_AUTH_EVENTBRITE_KEY = "pepe"
+SOCIAL_AUTH_EVENTBRITE_SECRET = "pepe"
