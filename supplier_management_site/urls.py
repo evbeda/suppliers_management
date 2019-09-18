cb466d0254f30458fce47e5334c20d3a93234597
nicocaif <ricardo.caif@ricardo.caif-10964>
2019-09-18 14:29:58 -0300

    [PATCH] EDA-532 [Investigation] i18n for the project

--- supplier_management_site/urls.py
+++ supplier_management_site/urls.py
@@ -2,10 +2,15 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = i18n_patterns(
+
+urlpatterns = [
+    url(r'^', include('users_app.urls')),
+    url('', include('social_django.urls', namespace='social'))
+]
+
+
+urlpatterns += i18n_patterns(
     url(r'^admin/', admin.site.urls),
     url(r'^suppliersite/', include('supplier_app.urls')),
     url(r'^apsite/', include('AP_app.urls')),
-    url(r'^', include('users_app.urls')),
-    url('', include('social_django.urls', namespace='social'))
 )
