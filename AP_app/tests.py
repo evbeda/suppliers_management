f201edfb475fdd8661f5bddc7158eed7b8fb3d69
Juan Pablo Vallejo <juan.vallejo@eventbrite.com>
2019-09-19 11:18:07 -0300

    Fixed oauth login and added permissions for ap-home

--- AP_app/tests.py
+++ AP_app/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import Client, TestCase
+from django.urls import reverse
+
+from users_app.models import User
+
+
+class TestAP(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(email='ap@eventbrite.com')
+        self.client = Client()
+        return super().setUp()
+
+    def test_ap_site_permission(self):
+        self.client.force_login(self.user)
+        response = self.client.get(
+            reverse('ap-home'),
+            follow=True,
+        )
+        self.assertEqual(response.status_code, HTTPStatus.OK)
--- AP_app/views.py
+++ AP_app/views.py
@@ -1,7 +1,15 @@
 from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+from . import ALLOWED_AP_ACCOUNTS
 
 
-class APHome(LoginRequiredMixin, TemplateView):
+class APHome(UserPassesTestMixin, LoginRequiredMixin, TemplateView):
     template_name = 'AP_app/ap-home.html'
     login_url = '/'
+
+    def test_func(self):
+        if self.request.user.is_authenticated():
+            return self.request.user.email in ALLOWED_AP_ACCOUNTS
+        else:
+            return False
