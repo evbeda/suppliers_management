35770c11711a35d99c541b260e4c01eb373db2bb
ricardocaif-eb <ricardo.caif@eventbrite.com>
2019-09-23 14:57:09 -0300

    [MINOR] EDA-558 Create company model

--- invoices_app/tests.py
+++ invoices_app/tests.py
@@ -1,9 +1,17 @@
 from django.test import TestCase
+from parameterized import parameterized
+from .models import Company
 
 
-class TestInvoices(TestCase):
+class TestModels(TestCase):
     def setUp(self):
         pass
 
-    def test_init(self):
-        self.assertTrue(True)
+    @parameterized.expand([
+        ('Eventbrite', 'Bringing the world together through live experiences')
+    ])
+    def test_company(self, name, description):
+        company = Company(name=name, description=description)
+        self.assertEqual(company.name, name)
+        self.assertEqual(company.description, description)
+        self.assertEqual(str(company), "Company:{} Description:{}".format(name, description))
