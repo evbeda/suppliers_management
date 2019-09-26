cec8a9b742f43413202eff0b227412a24b6406d1
Juan Pablo Vallejo <juan.vallejo@eventbrite.com>
2019-09-26 11:59:49 -0300

    Improved navbar and landing page

--- AP_app/urls.py
+++ AP_app/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from .views import APHome, InvoiceListView
 
 urlpatterns = [
-    url(r'^home$', APHome.as_view(), name='ap-home'),
-    url(r'^invoices$', InvoiceListView.as_view(), name='ap-invoices'),
+    url(r'^home$', APHome.as_view(), name='ap-welcome'),
+    url(r'^invoices$', InvoiceListView.as_view(), name='ap-home'),
 ]
