ba6c023e09b01ede9c0e995cebfa6fd2b1815623
julianandres <julian.andres@eventbrite.com>
2019-09-27 09:35:49 -0300

    New logout button

--- AP_app/__init__.py
+++ AP_app/__init__.py
@@ -1,1 +1,1 @@
-ALLOWED_AP_ACCOUNTS = ['ap@eventbrite.com', 'nicolas.nunez@eventbrite.com', 'juan.vallejo@eventbrite.com']
+ALLOWED_AP_ACCOUNTS = ['ap@eventbrite.com', 'nicolas.nunez@eventbrite.com', 'juan.vallejo@eventbrite.com', 'julian.andres@eventbrite.com']
