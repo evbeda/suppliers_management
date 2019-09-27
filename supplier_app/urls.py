853210c6ed21c4d2a2f000fafea46171d561f4d0
Nahuel <nahuel.valencia@eventbrite.com>
2019-09-27 08:57:17 -0300

    [PATCH] EDA-567 Button to redirect to invoices added

--- supplier_app/urls.py
+++ supplier_app/urls.py
@@ -16,10 +16,10 @@ from .views import (
 
 urlpatterns = [
     url(r'^home$', SupplierHome.as_view(), name='supplier-home'),
-    # url(r'^home/taxpayers$', SupplierHome.as_view(), name='supplier-home'),
     url(r'^files/create$', CreatePDFFileView.as_view(), name='create-file'),
     url(r'^files/$', PDFFileView.as_view(), name='show-files'),
     url(r'^taxpayer/create$', CreateTaxPayerView.as_view(), name='taxpayer-create'),
     url(r'^(?P<taxpayer_id>[0-9]+)/invoices/$', InvoiceListView.as_view(), name='supplier-invoice-list'),
-    url(r'^(?P<taxpayer_id>[0-9]+)/invoices/new/$', InvoiceCreateView.as_view(), name='invoice-create')
+    url(r'^(?P<taxpayer_id>[0-9]+)/invoices/new/$', InvoiceCreateView.as_view(), name='invoice-create'),
+    # url(r'^(?P<taxpayer_id>[0-9]+)/update/$', TaxPayerUpdateView.as_view(), name='supplier-update')
 ]
