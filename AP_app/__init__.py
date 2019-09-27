74d22fb7404619261c47aa592fd6082ab4ce59c8
julian.rofrano <julian.rofrano@eventbrite.com>
2019-09-27 15:02:32 -0300

    added kevin to ap allowed

--- AP_app/__init__.py
+++ AP_app/__init__.py
@@ -1,7 +1,9 @@
 ALLOWED_AP_ACCOUNTS = [
-    'ap@eventbrite.com',
-    'nicolas.nunez@eventbrite.com',
-    'juan.vallejo@eventbrite.com',
-    'nahuel.valencia@eventbrite.com',
+    'ap@eventbrite.com', 
+    'nicolas.nunez@eventbrite.com', 
+    'juan.vallejo@eventbrite.com', 
+    'julian.rofrano@eventbrite.com',
     'julian.andres@eventbrite.com',
+    'kgerez@eventbrite.com',
+    'nahuel.valencia@eventbrite.com',
 ]
