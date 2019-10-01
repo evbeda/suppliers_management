234b50c3c5fef97e611bad7f254659b2b5568144
Nahuel <nahuel.valencia@eventbrite.com>
2019-10-01 11:58:05 -0300

    [PATCH] EDA-549, EDA-546. List all new suppliers in Ap home

--- supplier_app/__init__.py
+++ supplier_app/__init__.py
@@ -2,7 +2,11 @@
 MAX_SIZE_FILE = 5242880
 ALLOWED_FILE_EXTENSIONS = ['.pdf']
 TAXPAYER_STATUS = [
-    ("PENDING", "Pending"),
     ("ACTIVE", "Active"),
     ("CHANGE REQUIRED", "Change required"),
+    ("PENDING", "Pending"),
+]
+TAXPAYER_STATUS_AP = [
+    'CHANGE REQUIRED',
+    'PENDING',
 ]
