88cfb361663c9ff4cdc2d40a4af1b049f504bb7d
Nahuel <nahuel.valencia@eventbrite.com>
2019-10-01 12:50:22 -0300

    [MAJOR] EDA-547 WIP: list suppliers details

--- supplier_app/urls.py
+++ supplier_app/urls.py
@@ -5,6 +5,7 @@ from supplier_app.views import (
     CreatePDFFileView,
     CreateTaxPayerView,
     PDFFileView,
+    SupplierDetailsView,
     SupplierHome,
 )
 
@@ -12,7 +13,8 @@ urlpatterns = [
     url(r'^ap$', ApTaxpayers.as_view(), name='ap-taxpayers'),
     url(r'^supplier$', SupplierHome.as_view(), name='supplier-home'),
     url(r'^supplier/taxpayer/create$', CreateTaxPayerView.as_view(), name='taxpayer-create'),
-    # url(r'^(?P<taxpayer_id>[0-9]+)/update/$', TaxPayerUpdateView.as_view(), name='supplier-update')
+    url(r'^taxpayer/(?P<taxpayer_id>[0-9]+)/details/$', SupplierDetailsView.as_view(), name='supplier-details'),
     url(r'^files/create$', CreatePDFFileView.as_view(), name='create-file'),
     url(r'^files/$', PDFFileView.as_view(), name='show-files'),
+    # url(r'^(?P<taxpayer_id>[0-9]+)/update/$', TaxPayerUpdateView.as_view(), name='supplier-update')
 ]
