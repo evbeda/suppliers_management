738244a03094e4cba44a089c1ea240b7c91c4411
Nahuel <nahuel.valencia@eventbrite.com>
2019-10-02 16:13:51 -0300

    [MAJOR] EDA-547 Details of a specific taxpayer

--- supplier_app/factory_boy.py
+++ supplier_app/factory_boy.py
@@ -1,8 +1,14 @@
 import factory
 from supplier_app.models import (
+    Address,
+    BankAccount,
     Company,
+    CompanyUserPermission,
     TaxPayer,
     TaxPayerArgentina,
+)
+from users_app.factory_boy import (
+    UserFactory,
 )
 
 
@@ -14,6 +20,15 @@ class CompanyFactory(factory.django.DjangoModelFactory):
         model = Company
 
 
+class CompanyUserPermissionFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = CompanyUserPermission
+
+    company = factory.SubFactory(CompanyFactory)
+    user = factory.SubFactory(UserFactory)
+
+
 class TaxPayerFactory(factory.django.DjangoModelFactory):
 
     class Meta:
@@ -23,6 +38,7 @@ class TaxPayerFactory(factory.django.DjangoModelFactory):
     name = factory.Sequence(lambda n: "Eventbrite {}".format(n))
     taxpayer_state = "PENDING"
     country = "AR"
+    company = factory.SubFactory(CompanyFactory)
 
 
 class TaxPayerArgentinaFactory(TaxPayerFactory):
@@ -34,3 +50,26 @@ class TaxPayerArgentinaFactory(TaxPayerFactory):
     cuit = "1234569"
     justificacion = "Justificacion falsa"
     forma_de_pago = "Banco"
+
+
+class AddressFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Address
+
+    street = "Calle falsa"
+    number = factory.Sequence(lambda n: "{}".format(n))
+    zip_code = "5500"
+    city = "Godoy Cruz"
+    state = "Mendoza"
+    country = "Argentina"
+    taxpayer = factory.SubFactory(TaxPayerFactory)
+
+
+class BankAccountFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = BankAccount
+
+    bank_name = "Galicia"
+    bank_code = factory.Sequence(lambda n: "{}".format(n))
+    account_number = "759267846132412"
+    taxpayer = factory.SubFactory(TaxPayerFactory)
