e6342886558d287e9a5d586b2aeaf6d53240c9a8
Nahuel <nahuel.valencia@eventbrite.com>
2019-10-08 15:20:49 -0300

    heroku qa environment

--- supplier_management_site/settings/production.py
+++ supplier_management_site/settings/production.py
@@ -2,7 +2,7 @@ from .base import *  # noqa
 import dj_database_url
 from supplier_management_site import get_env_variable
 
-ALLOWED_HOSTS = ['brite-su.herokuapp.com']
+ALLOWED_HOSTS = ['secure-plateau-22734.herokuapp.com']
 
 DATABASES = {
     'default': {
