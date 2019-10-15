19012a9775ccee40812b41b9ea9e80bccb27f475
julianandres <julian.andres@eventbrite.com>
2019-10-15 11:46:28 -0300

    Upload a file when posting a comment

---
+++ invoices_app/tests/test_comments.py
@@ -0,0 +1,437 @@
+from http import HTTPStatus
+from parameterized import parameterized
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.urls import reverse
+
+from invoices_app import (
+    INVOICE_STATUS_APPROVED,
+    INVOICE_STATUS_REJECTED,
+    INVOICE_STATUS_CHANGES_REQUEST,
+    INVOICE_STATUS_PAID,
+    INVOICE_MAX_SIZE_FILE
+)
+from invoices_app.factory_boy import InvoiceFactory
+from invoices_app.models import Comment
+from invoices_app.tests.test_base import TestBase
+
+from supplier_app.tests.factory_boy import CompanyUserPermissionFactory
+
+from users_app.factory_boy import UserFactory
+
+from utils.invoice_lookup import invoice_status_lookup
+
+class CommentsTest(TestBase):
+    # Feature: Generates comments when invoice state changes
+    @parameterized.expand([
+        (INVOICE_STATUS_APPROVED,),
+        (INVOICE_STATUS_CHANGES_REQUEST,),
+        (INVOICE_STATUS_REJECTED),
+        (INVOICE_STATUS_PAID,),
+    ])
+    def test_generate_a_comment_when_invoice_changes_his_state(
+        self,
+        new_status,
+    ):
+        # Given an invoice and a logged AP
+        # invoice : self.invoice
+        self.client.force_login(self.ap_user)
+
+        # When AP changes its state
+        response = self.client.post(
+            reverse(
+                'change-invoice-status',
+                kwargs={
+                    'pk': self.invoice.id,
+                }
+            ),
+            {'status': invoice_status_lookup(new_status)}
+        )
+        # Then the invoice should have a comment associated to it with its message
+        comment = Comment.objects.filter(
+            invoice=self.invoice
+        ).latest('comment_date_received')
+
+        self.assertEqual(
+            str(comment),
+            '{} has changed the invoice status to {}'.format(
+                self.ap_user.email,
+                new_status
+            )
+        )
+        self.assertEqual(comment.user, self.ap_user)
+        self.assertEqual(HTTPStatus.FOUND, response.status_code)
+
+    def test_supplier_invoice_edit_and_it_exists_a_new_comment(self):
+        # Given an invoice and a logged Supplier
+        # invoice : self.invoice
+        self.client.force_login(self.user)
+        self.invoice.status = invoice_status_lookup(INVOICE_STATUS_CHANGES_REQUEST)
+        self.invoice.save()
+
+        # When a supplier edits the invoice
+        response = self.client.post(
+            reverse(
+                'taxpayer-invoice-update',
+                kwargs={
+                    'taxpayer_id': self.taxpayer.id,
+                    'pk': self.invoice.id
+                }
+            ),
+            self.invoice_post_data
+        )
+        # Then the invoice should have a comment associated to it with its message
+        comment = Comment.objects.filter(
+            invoice = self.invoice
+        ).latest('comment_date_received')
+
+        self.assertEqual(
+            str(comment),
+            '{} has changed the invoice'.format(
+                self.user.email,
+            )
+        )
+        self.assertEqual(comment.user, self.user)
+        self.assertEqual(HTTPStatus.FOUND, response.status_code)
+
+    @parameterized.expand([
+        ('Valid text', HTTPStatus.FOUND),
+    ])
+    def test_ap_can_add_a_comment_in_an_invoice(self, message, expected_status_code):
+        # Given an invoice and a logged AP
+        # invoice : self.invoice
+        self.client.force_login(self.ap_user)
+
+        # When AP writes a comment
+        response = self.client.post(
+            reverse('post-comment',
+                kwargs={
+                    'pk': self.invoice.id,
+                },
+            ),
+            {'message': message}
+        )
+
+        # Then the invoice should have a comment associated to it with its message
+        comment = Comment.objects.filter(
+            invoice = self.invoice
+        ).latest('comment_date_received')
+
+        self.assertEqual(expected_status_code, response.status_code)
+        self.assertEqual(
+            comment.message,
+            '{} {}'.format(
+                self.ap_user,
+                message
+            )
+        )
+        self.assertEqual(comment.user, self.ap_user)
+
+    @parameterized.expand([
+        ('Valid text', HTTPStatus.FOUND),
+    ])
+    def test_supplier_can_add_a_comment_in_an_invoice(
+        self,
+        message,
+        expected_status_code
+    ):
+        # Given an invoice and a logged supplier
+        # invoice : self.invoice
+        self.client.force_login(self.user)
+
+        # When AP writes a comment
+        response = self.client.post(
+            reverse('post-comment',
+                kwargs={
+                    'pk': self.invoice.id,
+                },
+            ),
+            {'message': message}
+        )
+
+        # Then the invoice should have a comment associated to it with its message
+        comment = Comment.objects.filter(
+            invoice = self.invoice
+        ).latest('comment_date_received')
+
+        self.assertEqual(expected_status_code, response.status_code)
+        self.assertEqual(
+            comment.message,
+            '{} {}'.format(
+                self.user,
+                message
+            )
+        )
+        self.assertEqual(comment.user, self.user)
+
+    def test_post_an_empty_comment(self):
+        # Given an invoice and a logged AP
+        # invoice : self.invoice
+        self.client.force_login(self.ap_user)
+
+        # When AP writes an empty comment
+        response = self.client.post(
+            reverse('post-comment',
+                kwargs={
+                    'pk': self.invoice.id,
+                },
+            ),
+            {'message': ''}
+        )
+
+        # Then an error should appear
+        self.assertEqual(HTTPStatus.BAD_REQUEST, response.status_code)
+
+    @parameterized.expand([
+        ('Valid text', HTTPStatus.FOUND),
+    ])
+    def test_comment_to_an_invoice_within_the_company_and_same_tax_payer(
+        self,
+        message,
+        expected_status_code
+    ):
+        # Given an invoice and user (supplier)
+        # from other taxpayer but the same company
+        self.client.force_login(self.user)
+
+        self.user_for_same_taxpayer = UserFactory()
+        self.companyuserpermission = CompanyUserPermissionFactory(
+            company=self.company,
+            user=self.user_for_same_taxpayer
+        )
+        self.invoice = InvoiceFactory(
+            user=self.user,
+            taxpayer=self.taxpayer,
+            invoice_number='8695030'
+        )
+
+        # When the user posts a comment
+        response = self.client.post(
+            reverse('post-comment',
+                kwargs={
+                    'pk': self.invoice.id,
+                },
+            ),
+            {'message': message}
+        )
+
+        # Then the invoice should have a comment associated to it with its message
+        comment = Comment.objects.filter(
+            invoice = self.invoice
+        ).latest('comment_date_received')
+
+        self.assertEqual(expected_status_code, response.status_code)
+        self.assertEqual(
+            comment.message,
+            '{} {}'.format(
+                self.user,
+                message
+            )
+        )
+        self.assertEqual(comment.user, self.user)
+
+    def test_comment_to_an_invoice_not_of_the_company(self):
+        # Given an invoice and user (supplier)
+        # from other taxpayer but from other company
+        self.client.force_login(self.user)
+
+        # When the user posts a comment
+        response = self.client.post(
+            reverse('post-comment',
+                kwargs={
+                    'pk': self.invoice_from_other_user.id,
+                },
+            ),
+            {'message': 'Valid Text'}
+        )
+        # Then an error should appear
+        self.assertEqual(HTTPStatus.FORBIDDEN, response.status_code)
+
+    def test_post_a_comment_without_a_logged_user(self):
+        # Given an invoice and a not logged user
+        # invoice: self.invoice
+        # user: self.user
+
+        # When the user posts a comment
+        response = self.client.post(
+            reverse('post-comment',
+                kwargs={
+                    'pk': self.invoice.id,
+                },
+            ),
+            {'message': 'Valid Text'}
+        )
+
+        # Then an error should appear
+        self.assertEqual(HTTPStatus.FORBIDDEN, response.status_code)
+
+    def test_comment_redirects_to_invoice_detail_view(self):
+        # Given an invoice and a logged user
+        # invoice: self.invoice
+        self.client.force_login(self.user)
+
+        # When the user posts a comment
+        response = self.client.post(
+            reverse('post-comment',
+                kwargs={
+                    'pk': self.invoice.id,
+                },
+            ),
+            {'message': 'Valid Text'}
+        )
+
+        # Then it redirects to InvoiceDetailView
+        self.assertRedirects(
+            response,
+            '/invoices/taxpayer/{}/detail/{}/'.format(
+                self.invoice.taxpayer.id,
+                self.invoice.id,
+            )
+        )
+
+    @parameterized.expand([
+        ('Valid text', HTTPStatus.FOUND),
+    ])
+    def test_ap_can_add_a_comment_in_an_invoice_with_a_file(
+        self,
+        message,
+        expected_status_code,
+    ):
+        # Given an invoice and a logged AP
+        # invoice : self.invoice
+        self.client.force_login(self.ap_user)
+
+        # When AP writes a comment
+        response = self.client.post(
+            reverse('post-comment',
+                kwargs={
+                    'pk': self.invoice.id,
+                },
+            ),
+            {
+            'message': message,
+            'myFile': self.file_mock,
+            }
+        )
+
+        # Then the invoice should have a comment associated to it with its message
+        comment = Comment.objects.filter(
+            invoice = self.invoice
+        ).latest('comment_date_received')
+
+        self.assertEqual(expected_status_code, response.status_code)
+        self.assertEqual(
+            comment.message,
+            '{} {}'.format(
+                self.ap_user,
+                message
+            )
+        )
+        self.assertEqual(comment.user, self.ap_user)
+        self.assertTrue(self.file_mock.name in comment.comment_file.name)
+
+    @parameterized.expand([
+        ('Valid text', HTTPStatus.FOUND),
+    ])
+    def test_supplier_can_add_a_comment_in_an_invoice_with_a_file(
+        self,
+        message,
+        expected_status_code,
+    ):
+        # Given an invoice, a logged supplier and a file
+        # invoice : self.invoice
+        # file: self.file_mock
+        self.client.force_login(self.user)
+
+        # When supplier writes a comment
+        response = self.client.post(
+            reverse('post-comment',
+                kwargs={
+                    'pk': self.invoice.id,
+                },
+            ),
+            {
+            'message': message,
+            'myFile': self.file_mock
+            }
+        )
+
+        # Then the invoice should have a comment associated to it with its message
+        comment = Comment.objects.filter(
+            invoice = self.invoice
+        ).latest('comment_date_received')
+
+        self.assertEqual(expected_status_code, response.status_code)
+        self.assertEqual(
+            comment.message,
+            '{} {}'.format(
+                self.user,
+                message
+            )
+        )
+        self.assertEqual(comment.user, self.user)
+        self.assertTrue(self.file_mock.name in comment.comment_file.name)
+
+
+
+    @parameterized.expand([
+        ('test.xml', 20, ['Only .pdf allowed']),
+        ('test.pdf', 5242881, [
+            'The file size is greater than {}MB.'.format(int(INVOICE_MAX_SIZE_FILE/(1024*1024)))]),
+        ('test.xml', 5242881, [
+            'Only .pdf allowed', 'The file size is greater than {}MB.'.format(int(INVOICE_MAX_SIZE_FILE/(1024*1024)))])
+    ])
+    def test_supplier_adds_an_invalid_file_in_a_comment(
+        self,
+        name_file,
+        size_file,
+        messages
+    ):
+        # Given an invoice, a logged supplier and a file
+        # invoice : self.invoice
+        self.client.force_login(self.user)
+        # When supplier writes a comment
+        response = self.client.post(
+            reverse('post-comment',
+                kwargs={
+                    'pk': self.invoice.id,
+                },
+            ),
+            {
+            'message': 'Valid Text',
+            'myFile': SimpleUploadedFile(name_file, bytes(size_file)),
+            },
+            follow=True
+        )
+        # Then a message should appear
+        for message in messages:
+            self.assertContains(response, message)
+
+    @parameterized.expand([
+        ('test.xml', 20,),
+        ('test.pdf', 5242881,),
+        ('test.xml', 5242881,)
+    ])
+    def test_supplier_adds_an_invalid_file_in_a_comment_and_no_comment_is_created(
+        self,
+        name_file,
+        size_file,
+    ):
+        # Given an invoice, a logged supplier and a file
+        # invoice : self.invoice
+        self.client.force_login(self.user)
+        # When supplier writes a comment
+        response = self.client.post(
+            reverse('post-comment',
+                kwargs={
+                    'pk': self.invoice.id,
+                },
+            ),
+            {
+            'message': 'Valid Text',
+            'myFile': SimpleUploadedFile(name_file, bytes(size_file)),
+            },
+            follow=True
+        )
+        # Then no comment should be created
+        self.assertFalse(Comment.objects.all())
--- utils/file_validator.py
+++ utils/file_validator.py
@@ -1,4 +1,49 @@
-def validate_file(file, max_size_form):
-    return (True, '') \
-        if file.size <= max_size_form \
-        else (False, 'Your file is greater than {}.'.format(max_size_form))
+from invoices_app import (
+    INVOICE_FILE_FIELDS,
+    INVOICE_MAX_SIZE_FILE,
+    INVOICE_ALLOWED_FILE_EXTENSIONS,
+)
+
+
+def validate_file(file, max_size_form=None):
+    if not max_size_form:
+        max_size_form = INVOICE_MAX_SIZE_FILE
+
+    value_to_return = {'is_valid': True, "errors": []}
+    valid_content_type = False
+
+    if file.size >= max_size_form:
+            value_to_return['is_valid'] = False
+            value_to_return['errors'].append(
+                'The file size is greater than {}MB.'.format(int(max_size_form/(1024*1024)))
+            )
+
+    for i in INVOICE_ALLOWED_FILE_EXTENSIONS:
+        if file.name.endswith(i):
+            valid_content_type = True
+
+    if not valid_content_type:
+        value_to_return['is_valid'] = False
+        value_to_return['errors'].append(
+            'Only {} allowed'.format(
+                ''.join(INVOICE_ALLOWED_FILE_EXTENSIONS)
+            )
+    )
+    return list(value_to_return.values())
+
+
+
+def is_file_valid(self, valid, file_field):
+    if not valid:
+        return valid
+    for file_field in INVOICE_FILE_FIELDS:
+        file_data = self.cleaned_data[file_field]
+        if file_data:
+            file_is_valid, msg = validate_file(
+                file_data,
+                INVOICE_MAX_SIZE_FILE,
+            )
+            if not file_is_valid:
+                self.add_error(file_field, msg)
+                return file_is_valid
+    return valid
