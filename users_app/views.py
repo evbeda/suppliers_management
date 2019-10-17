94fcddf96b319e0d19d21caed6c1e560374c6fbf
Juan Pablo Vallejo <juan.vallejo@eventbrite.com>
2019-10-17 10:51:55 -0300

    [EDA-587] View to manage admin users

--- users_app/views.py
+++ users_app/views.py
@@ -1,7 +1,10 @@
 from django.views.generic import TemplateView
+from django.views.generic.list import ListView
 from django.contrib.auth.views import LogoutView
-from django.contrib.auth.mixins import UserPassesTestMixin
-from users_app import ALLOWED_AP_ACCOUNTS
+from django.contrib.auth.mixins import PermissionRequiredMixin, UserPassesTestMixin
+
+from users_app import ALLOWED_AP_ACCOUNTS, CAN_MANAGE_APS_PERM
+from users_app.models import User
 
 
 class LoginView(TemplateView):
@@ -21,3 +24,13 @@ class IsApUser(UserPassesTestMixin):
 
     def test_func(self):
         return self.request.user.email in ALLOWED_AP_ACCOUNTS
+
+
+class AdminList(PermissionRequiredMixin, ListView):
+    model = User
+    template_name = 'AP_app/admins-list.html'
+    permission_required = CAN_MANAGE_APS_PERM
+
+    def get_queryset(self):
+        queryset = User.objects.filter(email__endswith='@eventbrite.com')
+        return queryset
