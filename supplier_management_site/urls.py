d0a90b1f7f6ac8a02baae59299d1de18e988935b
ricardocaif-eb <ricardo.caif@eventbrite.com>
2019-10-22 16:33:28 -0300

    implemented dropdown list for selecting language on templates

--- supplier_management_site/urls.py
+++ supplier_management_site/urls.py
@@ -1,16 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.conf.urls.i18n import i18n_patterns
-
 
 urlpatterns = [
-]
-
-urlpatterns += i18n_patterns(
+    url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^invoices/', include('invoices_app.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^suppliersite/', include('supplier_app.urls')),
     url(r'^', include('users_app.urls')),
     url('', include('social_django.urls', namespace='social')),
-    prefix_default_language=False
-)
+]
