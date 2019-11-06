dde07277ab60e9d1fe7bc6444267046c3d38f30a
nicolas.nunez <nicolas.nunez@eventbrite.com>
2019-11-06 15:45:16 -0300

    Workday id refactor: It doesn't show anymore on the edit view, now there is a field next to the Approve button

--- supplier_app/change_status_strategy.py
+++ supplier_app/change_status_strategy.py
@@ -1,11 +1,23 @@
+from django.contrib import messages
+
+from supplier_app.exceptions.taxpayer_exceptions import NoWorkdayIDException
+from supplier_app.constants.custom_messages import (
+    TAXPAYER_APPROVE_MESSAGE,
+    TAXPAYER_DENIED_MESSAGE,
+    TAXPAYER_REQUEST_CHANGE_MESSAGE,
+)
 from utils.send_email import taxpayer_notification
+
 
 
 class StrategyStatusChange():
     def send_email(taxpayer):
         raise NotImplementedError()
 
-    def change_taxpayer_status(taxpayer):
+    def change_taxpayer_status(taxpayer, request):
+        raise NotImplementedError()
+
+    def show_message(request):
         raise NotImplementedError()
 
 
@@ -14,9 +26,12 @@ class StrategyDeny(StrategyStatusChange):
     def send_email(taxpayer):
         taxpayer_notification(taxpayer, 'taxpayer_denial')
 
-    def change_taxpayer_status(taxpayer):
+    def change_taxpayer_status(taxpayer, request=None):
         taxpayer.deny_taxpayer()
         taxpayer.save()
+
+    def show_message(request):
+        messages.success(request, TAXPAYER_DENIED_MESSAGE)
 
 
 class StrategyApprove(StrategyStatusChange):
@@ -24,9 +39,17 @@ class StrategyApprove(StrategyStatusChange):
     def send_email(taxpayer):
         taxpayer_notification(taxpayer, 'taxpayer_approval')
 
-    def change_taxpayer_status(taxpayer):
+    def change_taxpayer_status(taxpayer, request=None):
+        try:
+            workday_id = request.POST['workday_id']
+            taxpayer.workday_id = workday_id
+        except KeyError:
+            raise NoWorkdayIDException()
         taxpayer.approve_taxpayer()
         taxpayer.save()
+
+    def show_message(request):
+        messages.success(request, TAXPAYER_APPROVE_MESSAGE)
 
 
 class StrategyChangeRequired(StrategyStatusChange):
@@ -34,9 +57,12 @@ class StrategyChangeRequired(StrategyStatusChange):
     def send_email(taxpayer):
         taxpayer_notification(taxpayer, 'taxpayer_change_required')
 
-    def change_taxpayer_status(taxpayer):
+    def change_taxpayer_status(taxpayer, request=None):
         taxpayer.change_required_taxpayer()
         taxpayer.save()
+
+    def show_message(request):
+        messages.success(request, TAXPAYER_REQUEST_CHANGE_MESSAGE)        
 
 
 strategy = {
