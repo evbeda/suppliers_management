e3ed300e656325902198f95767fd45e0a21e1b78
stefania arias <sarias@eventbrite.com>
2020-05-12 17:58:05 -0300

    eb login for ar without breaking tests

--- supplier_management_site/auth/eventbrite_ar.py
+++ supplier_management_site/auth/eventbrite_ar.py
@@ -4,15 +4,25 @@ from django.conf import settings
 from django.views import i18n
 from django.utils.translation import to_locale, get_language
 
+
 class EventbriteAuth2(EventbriteOAuth2):
     AUTHORIZATION_URL = 'https://www.eventbrite.com/oauth/authorize'
     ACCESS_TOKEN_URL = 'https://www.eventbrite.com/oauth/token'
     METADATA_URL = 'https://www.eventbriteapi.com/v3/users/me'
 
-    def __init__(self, strategy, redirect_uri=None):
-        super().__init__(strategy, redirect_uri)
-        if to_locale(get_language()) == 'es' or strategy.session._session['_language'] == 'es':
+    def authorization_url(self):
+        if to_locale(get_language()) == 'es':
             self.AUTHORIZATION_URL = 'https://www.eventbrite.com.ar/oauth/authorize'
-            self.ACCESS_TOKEN_URL = 'https://www.eventbrite.com.ar/oauth/token'
-            self.METADATA_URL = 'https://www.eventbriteapi.com.ar/v3/users/me'
+        return self.AUTHORIZATION_URL
 
+    def access_token_url(self):
+        if to_locale(get_language()) == 'es':
+            self.ACCESS_TOKEN_URL = 'https://www.eventbrite.com.ar/oauth/token'
+        return self.ACCESS_TOKEN_URL
+
+    def user_data(self, access_token, *args, **kwargs):
+        if to_locale(get_language()) == 'es':
+            self.METADATA_URL = 'https://www.eventbriteapi.com.ar/v3/users/me'
+        return self.get_json(self.METADATA_URL, headers={
+          'Authorization': 'Bearer ' + access_token
+        })
