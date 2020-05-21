fe8c749d33a2710b28d4e3d47f914cfa0f3fc603
Axel Fernandez <axelfernandez@eventbrite.com>
2020-05-21 13:28:15 -0300

    adding fix result

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
 from django.utils.translation import ugettext_lazy as _
 
 GO_TO_BRITESU = _('Go to BriteSu')
-COMMENT_TAXPAYER = _('{} The field {} change from {} to {} \n')
+COMMENT_TAXPAYER = _('{} The field {} changed from {} to {} \n')
 COMMENT_TAXPAYER_INT_VALUES = _('{} The field {} has changed  \n')
