99f88ebbea177f97d582c4d6e44726ad64045a38
stefania arias <sarias@eventbrite.com>
2020-06-01 12:32:18 -0300

    Removing super user from admin list and adding translate

--- users_app/views.py
+++ users_app/views.py
@@ -84,13 +84,15 @@ class AdminList(PaginationMixin, PermissionRequiredMixin, ListView):
     def get_queryset(self):
         self.change_filter()
         if self.filter:
-            perm = Permission.objects.get(codename='ap_role')
+            perm_ap = Permission.objects.get(codename='ap_role')
             queryset = User.objects.filter(
-                Q(groups__permissions=perm)).distinct()
+                Q(groups__permissions=perm_ap)).distinct()
         else:
-            perm = Permission.objects.get(codename='supplier_role')
-            queryset = User.objects.filter(
-            ~Q(groups__permissions=perm)).distinct()
+            perm_ap = Permission.objects.get(codename='ap_role')
+            perm_buyer = Permission.objects.get(codename='buyer_role')
+            queryset = User.objects.filter(	queryset = User.objects.filter(
+                Q(groups__permissions=perm_buyer) | Q(groups__permissions=perm_ap)
+            ).distinct())
         return queryset
 
     def get_context_data(self, **kwargs):
