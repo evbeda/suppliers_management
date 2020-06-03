2289bc6575b4793e50c8dc22c10ebddd7330a2ef
stefania arias <sarias@eventbrite.com>
2020-06-03 15:33:07 -0300

    refactoring test

--- invoices_app/tests/test_comments.py
+++ invoices_app/tests/test_comments.py
@@ -515,68 +515,32 @@ class CommentsTest(TestBase):
         self.assertEqual(HTTPStatus.OK, response.status_code)
         self.assertContains(response, NO_COMMENT_ERROR)
 
-    @parameterized.expand([
-        ('False',),
-    ])
-    @patch('invoices_app.views._send_email_when_change_invoice_status')
-    def test_supplier_comment_new_state_not_generate_extra_comment_with_new_state(
-        self,
-        new_status,
-        __,
-    ):
+    def test_supplier_comment_new_state_not_generate_extra_comment_with_new_state(self):
         # Given an invoice and a logged supplier
         # invoice : self.invoice
         self.client.force_login(self.user)
-
-        # When supplier writes a comment
-        self.client.post(
-            reverse(
-                'post-comment',
-                kwargs={
-                    'pk': self.invoice.id,
-                },
-            ),
-            {'new_comment_from_supplier': invoice_status_lookup(new_status)}
+        # When supplier writes a comment the boolean turns True
+        self.invoice.new_comment_from_ap = True
+        self.invoice.save()
+        # Redirect to reload the new comment
+        self.client.get(
+            reverse('invoices-detail', kwargs={'taxpayer_id': self.taxpayer.id, 'pk': self.invoice.id}),
         )
-
-        # Then the invoice should have a comment associated to it with its message
-        comment = invoice_history_comments(self.invoice)
-
-        self.assertEqual(
-            len(Comment.objects.filter(invoice=self.invoice)),
-            0
-        )
+        # Then the invoice should not have a comment associated
+        comment = Comment.objects.all()
         self.assertEqual(len(comment), 0)
 
-    @parameterized.expand([
-        ('False',),
-    ])
-    @patch('invoices_app.views._send_email_when_change_invoice_status')
-    def test_ap_comment_new_state_not_generate_extra_comment_with_new_state(
-            self,
-            new_status,
-            __,
-    ):
-        # Given an invoice and a logged supplier
+    def test_ap_comment_new_state_not_generate_extra_comment_with_new_state(self):
+        # Given an invoice and a logged ap
         # invoice : self.invoice
         self.client.force_login(self.ap_user)
-
-        # When ap writes a comment
-        self.client.post(
-            reverse(
-                'post-comment',
-                kwargs={
-                    'pk': self.invoice.id,
-                },
-            ),
-            {'new_comment_from_ap': invoice_status_lookup(new_status)}
+        # When ap writes a comment the value of boolean turns True
+        self.invoice.new_comment_from_ap = True
+        self.invoice.save()
+        # Redirect to reload the new comment
+        self.client.get(
+            reverse('invoices-detail', kwargs={'taxpayer_id': self.taxpayer.id, 'pk': self.invoice.id}),
         )
-
-        # Then the invoice should have a comment associated to it with its message
-        comment = invoice_history_comments(self.invoice)
-
-        self.assertEqual(
-            len(Comment.objects.filter(invoice=self.invoice)),
-            0
-        )
+        # Then the invoice should not have a comment associated
+        comment = Comment.objects.all()
         self.assertEqual(len(comment), 0)
